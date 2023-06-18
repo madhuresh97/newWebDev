47b46a71beb20991013cae5531eaf01bb03c3d3f
Madhuresh Gupta <madhuresh97@yahoo.co.in>
2023-06-18 18:11:31 -0400

    Changes to the file structure

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import authRoute from "./routes/auth.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
+import usersRoute from "./routes/users.js"
 
 const app = express()
 dotenv.config()
@@ -18,9 +22,14 @@ mongoose.connection.on("disconnected", () => {
     console.log("MongoDB disconnected");
 });
 
-mongoose.connection.on("connected", () => {
-    console.log("MongoDB connected");
-});
+// Middleware
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
 
 app.get("/", (req, res) => {
     res.send("Hello First Request!")
